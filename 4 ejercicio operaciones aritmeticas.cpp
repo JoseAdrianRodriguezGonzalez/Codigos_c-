50a2eb1a085935dcb887e85d2964260957e9c3ed
JoseAdrianRodriguezGonzalez <88957069+JoseAdrianRodriguezGonzalez@users.noreply.github.com>
2023-06-18 21:37:53 -0600

    Add files via upload

---
+++ 4 ejercicio operaciones aritmeticas.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int numero1,numero2;
+	double suma =0, resta=0, multiplicacion =0, division =0;
+	cout<<"Digite un numero ";
+	cin>> numero1;
+	cout<< "digite un numero ";
+	cin>> numero2;
+	cout<<"El resultado es ";
+	suma=numero1+numero2;
+	resta=numero1-numero2;
+	multiplicacion=numero1*numero2;
+	division=numero1/numero2;
+	cout<<"suma: "<<suma<<endl;
+	cout<<"resta: "<<resta<<endl;
+	cout<<"multiplicacion: "<<multiplicacion<<endl;
+	cout<<"division "<<division<<endl;
+}
